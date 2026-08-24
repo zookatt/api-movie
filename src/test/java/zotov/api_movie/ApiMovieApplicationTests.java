1b9de993cb753f97880dcdf640e296850dc500e2
zookat <zotova.ea@gmail.com>
2026-08-24 10:29:01 +0200

    init: first commit

---
+++ src/test/java/zotov/api_movie/ApiMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package zotov.api_movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
