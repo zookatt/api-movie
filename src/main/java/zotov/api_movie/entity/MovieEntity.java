f281b7a0fb235aa12c72164680ae2c49cb38062e
Ekaterina Zotova <139338863+zookatt@users.noreply.github.com>
2026-08-25 12:53:44 +0200

    feat(entities):add movie entity

---
+++ src/main/java/zotov/api_movie/entity/MovieEntity.java
@@ -0,0 +1,52 @@
+package zotov.api_movie.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "movies")
+public class MovieEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private Integer year;
+
+    public MovieEntity() {
+    }
+
+    public MovieEntity(Long id, String title, Integer year) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+}
