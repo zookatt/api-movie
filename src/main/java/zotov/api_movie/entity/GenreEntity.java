b19f6c82d0a0b50799099057ef4b9e07883d43fd
Ekaterina Zotova <139338863+zookatt@users.noreply.github.com>
2026-08-25 12:57:33 +0200

    feat(entities): add genre entity

---
+++ src/main/java/zotov/api_movie/entity/GenreEntity.java
@@ -0,0 +1,42 @@
+package zotov.api_movie.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "genres")
+public class GenreEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    public GenreEntity() {
+    }
+
+    public GenreEntity(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
