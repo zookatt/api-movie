dea87c430625dc9d693b54907fabcfb6a2ec3cba
Ekaterina Zotova <139338863+zookatt@users.noreply.github.com>
2026-08-25 13:01:09 +0200

    feat(entities): add release year entity

---
+++ src/main/java/zotov/api_movie/entity/ReleaseYearEntity.java
@@ -0,0 +1,42 @@
+package zotov.api_movie.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "years")
+public class ReleaseYearEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer value;
+
+    public ReleaseYearEntity() {
+    }
+
+    public ReleaseYearEntity(Long id, Integer value) {
+        this.id = id;
+        this.value = value;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public void setValue(Integer value) {
+        this.value = value;
+    }
+}
