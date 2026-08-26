a9782eef68b4628ad932802907b719102c9e884c
zookat <zotova.ea@gmail.com>
2026-08-26 11:32:10 +0200

    fix(service): fix movie service implementation methods

--- src/main/java/zotov/api_movie/service/MovieServiceImpl.java
+++ src/main/java/zotov/api_movie/service/MovieServiceImpl.java
@@ -1,22 +1,53 @@
 package zotov.api_movie.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
+import zotov.api_movie.dto.MovieDTORequest;
+import zotov.api_movie.dto.MovieDTOResponse;
+import zotov.api_movie.mapper.MovieMapper;
 import zotov.api_movie.repository.MovieRepository;
 
 @Service
-public class MovieServiceImpl implements MovieService{
+public class MovieServiceImpl implements MovieService {
+
     private final MovieRepository movieRepository;
 
     public MovieServiceImpl(MovieRepository movieRepository) {
         this.movieRepository = movieRepository;
     }
 
-    private void findAll() {
-
+    @Override
+    public List<MovieDTOResponse> findAll() {
+        return movieRepository.findAll()
+                .stream()
+                .map(MovieMapper::toDTO)
+                .toList();
     }
 
-    private void findById() {
+    @Override
+    public Optional<MovieDTOResponse> findById(Long id) {
+        return movieRepository.findById(id)
+                .map(MovieMapper::toDTO);
+    }
 
+    @Override
+    public MovieDTOResponse create(MovieDTORequest dto) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'create'");
+    }
+
+    @Override
+    public Optional<MovieDTOResponse> update(Long id, MovieDTORequest dto) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    @Override
+    public boolean deleteById(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteById'");
     }
 }
