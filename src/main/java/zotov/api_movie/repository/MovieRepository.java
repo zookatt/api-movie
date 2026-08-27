3cb1ab7fd78c13814b408e77f5a7747f904a0dd6
Ekaterina Zotova <139338863+zookatt@users.noreply.github.com>
2026-08-27 12:38:41 +0200

    fix(repository): change import

--- src/main/java/zotov/api_movie/repository/MovieRepository.java
+++ src/main/java/zotov/api_movie/repository/MovieRepository.java
@@ -4,11 +4,10 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import zotov.api_movie.entity.GenreEntity;
 import zotov.api_movie.entity.MovieEntity;
 
 public interface MovieRepository extends JpaRepository<MovieEntity, Long> {
     List<MovieEntity> findByTitleContainingIgnoreCase(String title);
 
-    List<GenreEntity> findDistinctByGenresNameContainingIgnoreCase(String genre);
+    List<MovieEntity> findDistinctByGenresNameContainingIgnoreCase(String genre);
 }
