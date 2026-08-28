01a91fa6054f1b16a62214a2f24f7342fb101ae0
Ekaterina Zotova <139338863+zookatt@users.noreply.github.com>
2026-08-28 12:46:32 +0200

    feat(repository): add release year

--- src/main/java/zotov/api_movie/repository/ReleaseYearRepository.java
+++ src/main/java/zotov/api_movie/repository/ReleaseYearRepository.java
@@ -2,6 +2,8 @@ package zotov.api_movie.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import zotov.api_movie.entity.ReleaseYearEntity;
+import java.util.Optional;
 
 public interface ReleaseYearRepository extends JpaRepository<ReleaseYearEntity, Long> {
+    Optional<ReleaseYearEntity> findByValue(Integer value);
 }
