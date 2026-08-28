8f1715e36880ee22de42a06443bbc785d5565a14
Ekaterina Zotova <139338863+zookatt@users.noreply.github.com>
2026-08-28 11:18:49 +0200

    test(repository): fix movie search by genre

--- src/test/java/zotov/api_movie/repository/MovieRepositoryTest.java
+++ src/test/java/zotov/api_movie/repository/MovieRepositoryTest.java
@@ -3,9 +3,13 @@ package zotov.api_movie.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+
+import zotov.api_movie.entity.GenreEntity;
 import zotov.api_movie.entity.MovieEntity;
 
 import java.util.Optional;
+import java.util.List;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -15,6 +19,8 @@ class MovieRepositoryTest {
 
     @Autowired
     private MovieRepository movieRepository;
+    @Autowired
+    private GenreRepository genreRepository;
 
     @Test
     void shouldSaveAndFindMovieById() {
@@ -24,5 +30,17 @@ class MovieRepositoryTest {
         assertTrue(foundMovie.isPresent());
         assertEquals("Lost in Translation", foundMovie.get().getTitle());
         assertEquals(2003, foundMovie.get().getYear());
+    }
+
+    @Test
+    void shouldFindMoviesByGenreIgnoringCase() {
+        GenreEntity genre = genreRepository.save(new GenreEntity(null, "Drama"));
+        MovieEntity movie = new MovieEntity(null, "Lost in Translation", 2003);
+        movie.setGenres(Set.of(genre));
+        movieRepository.saveAndFlush(movie);
+        List<MovieEntity> movies = movieRepository
+                .findDistinctByGenresNameContainingIgnoreCase("dra");
+        assertEquals(1, movies.size());
+        assertEquals("Lost in Translation", movies.get(0).getTitle());
     }
 }
